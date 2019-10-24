9eb6630957a06e954569a6249a40ee03d1c812eb
photonn <photonn@hotmail.com>
2019-10-24 14:02:45 +0200

    no changes

--- two-fer/two_fer_test.sh
+++ two-fer/two_fer_test.sh
@@ -22,21 +22,21 @@
 }
 
 @test "a name given" {
-  #[[ $BATS_RUN_SKIPPED == true  ]] || skip
+  [[ $BATS_RUN_SKIPPED == true  ]] || skip
   run bash two_fer.sh Alice
   [[ $status -eq 0 ]]
   [[ $output == "One for Alice, one for me." ]]
 }
 
 @test "another name given" {
-  #[[ $BATS_RUN_SKIPPED == true  ]] || skip
+  [[ $BATS_RUN_SKIPPED == true  ]] || skip
   run bash two_fer.sh Bob
   [[ $status -eq 0 ]]
   [[ $output == "One for Bob, one for me." ]]
 }
 
 @test "handle arg1 properly" {
-  #[[ $BATS_RUN_SKIPPED == true  ]] || skip
+  [[ $BATS_RUN_SKIPPED == true  ]] || skip
   run bash two_fer.sh "John Smith" "Mary Ann"
   [[ $status -eq 0 ]]
   [[ $output == "One for John Smith, one for me." ]]
